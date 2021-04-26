6f65c36bf16cc6f9fc2847a21d128d3f78a2d191
Prashanth Kumar <prasxanth.kumar@gmail.com>
2021-04-26 11:51:39 -0700

    Updated docs/build.lisp docstring to conform to spec.

--- docs/build.lisp
+++ docs/build.lisp
@@ -2,7 +2,29 @@
 
 (defun build ()
   "
-Uses `MGL-PAX:UPDATE-ASDF-SYSTEM*` to generate `README` and `README.md` in the project root directory and `index.html` in the `docs/` sub-directory.
+**Synopsis** -- Generate MGL-PAX documentation from docstrings and documentation files
+
+**Input Arguments**
+
+No input arguments.
+
+**Return Values**
+
+No return values.
+
+**Description**
+
+Uses `MGL-PAX:UPDATE-ASDF-SYSTEM*` to generate,
+
+* `README` and `README.md` in the project root directory
+* `index.html` in the `docs/` sub-directory.
+
+**Examples**
+
+```
+(ql:quickload :sxablono/docs)
+(sxdocs:build)
+```
   "
 
   ;; Update READMEs
